d284b43f19bc0f94f3a052a8e0c2d470482eff90
shobit1337 <shobitdeshwal1337@gmail.com>
2021-04-16 21:36:48 +0530

    added firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCu0GSx_1YU_WYQXY3mxZkenyAHIci5hQM",
+  authDomain: "pixabook-tminus5.firebaseapp.com",
+  projectId: "pixabook-tminus5",
+  storageBucket: "pixabook-tminus5.appspot.com",
+  messagingSenderId: "605236583948",
+  appId: "1:605236583948:web:ed934ab98105a93b6a1829",
+  measurementId: "G-WGHKEEG71C",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const serverStorage = firebase.storage();
+const serverFirestore = firebase.firestore();
+
+export { serverStorage, serverFirestore };
