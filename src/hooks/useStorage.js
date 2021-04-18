1ede926b9bc4f472397500be1e52f7e4da397042
shobit1337 <shobitdeshwal1337@gmail.com>
2021-04-18 16:33:29 +0530

    Storing data to storage

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { serverStorage, serverFirestore, timestamp } from "../firebase/config";
+
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    const storageRef = serverStorage.ref(file.name);
+    const collectionRef = serverFirestore.collection("images");
+
+    storageRef.put(file).on(
+      "state_changed",
+      (snap) => {
+        let uploadPercentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(uploadPercentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        const createdAt = timestamp();
+        collectionRef.add({ url, createdAt });
+        setUrl(url);
+      }
+    );
+  }, [file]);
+
+  return { progress, url, error };
+};
+export default useStorage;
