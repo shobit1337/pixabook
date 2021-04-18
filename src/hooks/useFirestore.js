d6867e7e0ede867d1d10f14fc5ecdeccec6e68a8
shobit1337 <shobitdeshwal1337@gmail.com>
2021-04-18 18:12:59 +0530

    Added useFirestore hook

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { serverFirestore } from "../firebase/config";
+const useFirestore = (collection) => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const unsub = serverFirestore
+      .collection(collection)
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snap) => {
+        const document = [];
+        snap.forEach((doc) => {
+          document.push({ ...doc.data(), id: doc.id });
+        });
+        setDocs(document);
+      });
+    return () => unsub;
+  }, [collection]);
+
+  return { docs };
+};
+export default useFirestore;
