ec5d4175e05b939e04aedb3c3275ae5e634f4f1c
shobit1337 <shobitdeshwal1337@gmail.com>
2021-04-27 08:34:00 +0530

    Changed to class based

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-const App = () => {
-  return <div></div>;
-};
-
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
 export default App;
