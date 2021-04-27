5d2edbe52ec716f9ada2190ea699e01a96705eb8
shobit1337 <shobitdeshwal1337@gmail.com>
2021-04-27 08:33:41 +0530

    Added firebase config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyCXv3V_rYVPfRgtpr0HyBxi_N00L8TRVAA',
+  authDomain: 'your-notepad-1337.firebaseapp.com',
+  projectId: 'your-notepad-1337',
+  storageBucket: 'your-notepad-1337.appspot.com',
+  messagingSenderId: '595607305918',
+  appId: '1:595607305918:web:84b04b5eeff65b2151e8ef',
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
